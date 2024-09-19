d36a4d64f58b922e90bc7eda923e9d01f3904350
Wendelltmo <wendelltmo2306@outlook.com>
2024-09-19 20:31:31 -0300

    Initial commit

---
+++ app/src/main/java/page/x/PageX.java
@@ -0,0 +1,8 @@
+package page.x;
+
+public class PageX {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to PageX!");
+    }
+}
---
+++ app/src/test/java/page/x/AppTest.java
@@ -0,0 +1,11 @@
+package page.x;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test 
+    void appHasAGreeting() {
+        assertTrue(true);
+    }
+}
