248de6d167c78931a793ec1c1497ba7fa9e56253
Wendelltmo <wendelltmo2306@outlook.com>
2024-09-21 23:21:01 -0300

    feat: SeconChance impl

---
+++ app/src/main/java/page/x/algoritmos/substituicao/SecondChance.java
@@ -0,0 +1,54 @@
+package page.x.algoritmos.substituicao;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+import page.x.TlbEntry;
+import page.x.interruptions.MissInterruption;
+import page.x.utils.Pair;
+
+public class SecondChance implements AlgoritmoSubstituicaoI {
+
+    private int quantidadeEntries;
+
+    private Queue<Pair<Boolean, TlbEntry>> entries;
+
+    public SecondChance(int quantidadeEntries) {
+        this.quantidadeEntries = quantidadeEntries;
+        this.entries = new LinkedList<>();
+    }
+
+    @Override
+    public Integer mapearPagina(Integer vpn) throws MissInterruption {
+        for (Pair<Boolean, TlbEntry> pairAtual : entries) {
+            TlbEntry entryAtual = pairAtual.getPair2();
+            if (entryAtual.getVirtualPageNumber().equals(vpn)) {
+                pairAtual.setPair1(true);
+                return entryAtual.getPageFrameNumber();
+            }
+        }
+        throw new MissInterruption();
+    }
+
+    @Override
+    public void addPaginaMapeada(Integer vpn, Integer pfn) {
+        TlbEntry tlbEntry = new TlbEntry(vpn, pfn);
+        Pair<Boolean, TlbEntry> tlbEntryPair = new Pair<Boolean,TlbEntry>(false, tlbEntry);
+        if (entries.size() == quantidadeEntries) {
+            this.removePagina();
+        }
+        this.entries.add(tlbEntryPair);
+        
+    }
+
+    private void removePagina() {
+        Pair<Boolean, TlbEntry> tlbEntryPair = this.entries.poll();
+        while(tlbEntryPair.getPair1()) {
+            tlbEntryPair.setPair1(false);
+            this.entries.add(tlbEntryPair);
+            tlbEntryPair = this.entries.remove();
+        }
+    }
+
+    
+}
